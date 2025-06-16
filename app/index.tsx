bbd877b131e041dfc8af4ea54998c08339da4d2d
jericho-millena <millenajerichojames@gmail.com>
2025-06-16 14:27:49 +0800

    Start

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Landing() {
+  return (
+    <View className="bg-blue-800 p-4 justify-center items-center flex-1">
+      <Text className="text-6xl font-bold text-white">Fitly</Text>
+    </View>
+  );
+}
---
+++ components/greeting.tsx
@@ -0,0 +1,15 @@
+// components/Greeting.tsx
+import React from "react";
+import { Text, View } from "react-native";
+
+const Greeting = () => {
+  return (
+    <View className="bg-blue-100 p-4 rounded-lg">
+      <Text className="text-xl font-bold text-blue-800">
+        Hello from SDK 50+ 🎉
+      </Text>
+    </View>
+  );
+};
+
+export default Greeting;
